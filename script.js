ff29aba40a695116d5af2908b92a7dcd5069dabd
MannyBinning <manib58@live.co.uk>
2020-12-14 23:03:39 +0000

    Updated style css, added score button to save high scores of the game.

---
+++ script.js
@@ -0,0 +1,25 @@
+const question = document.getElementById('question')
+const answers = Array.from(document.getElementById('answers'))
+const scoreText = document.getElementById('score')
+
+let currentQuestion = {}
+let acceptAnswers = true
+let score = 0
+let questionCounter = 0 
+let availableQuestions = {}
+
+let questions = {
+}
+
+function startGame(){
+
+}
+
+function setNextQuestion(){
+
+}
+
+function selectAnswer(){
+
+}
+
