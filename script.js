7dd405089170bc04ea62a8670aac2839dd167f57
MannyBinning <manib58@live.co.uk>
2020-12-19 00:31:25 +0000

    Deleted Cars page and Sports page to make the quiz in one page for better quality. Also, added example questions in js

--- script.js
+++ script.js
@@ -5,21 +5,46 @@ const scoreText = document.getElementById('score')
 let currentQuestion = {}
 let acceptAnswers = true
 let score = 0
-let questionCounter = 0 
-let availableQuestions = {}
+let availableQuestions = []
 
-let questions = {
-}
+let questions = [
+    {
+    question: 'What is 3?',
+    answers: [
+      { text: '3', correct: true },
+      { text: '22', correct: false },
+      { text: '8', correct: false },
+      { text: '24', correct: false }
+    ]
+  },
+  {
+    question: 'Who is 5?',
+    answers: [
+      { text: '6', correct: false },
+      { text: '4', correct: false },
+      { text: '5', correct: true },
+      { text: '1', correct: false }
+    ]
+  },
+  {
+    question: 'what is this?',
+    answers: [
+      { text: 'this', correct: true },
+      { text: 'that', correct: false },
+      { text: 'those', correct: false },
+      { text: 'what', correct: false }
+    ]
+  },
+  {
+    question: 'What is 16?',
+    answers: [
+      { text: '10', correct: false },
+      { text: '16', correct: true },
+      { text: '22', correct: false },
+      { text: '22', correct: false }
+    ]
+  }
+]
 
-function startGame(){
 
-}
-
-function setNextQuestion(){
-
-}
-
-function selectAnswer(){
-
-}
 
