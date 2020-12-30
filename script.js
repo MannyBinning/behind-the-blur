d09fec2f5db1db6da2cb9f4cb30a29b415e30a34
MannyBinning <manib58@live.co.uk>
2020-12-30 12:32:17 +0000

    Added images with blur and updated hints section with better information and styling

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-    
     const nextQuestion = document.querySelector('#next-btn')
-    const imageAsked = document.querySelector('#image')
     const hintAsked = document.querySelector ('#hint')
     const questionContainer = document.querySelector('#question-container')
     const questionAsked = document.querySelector('#question')
     const answerButtons = document.querySelector('#answer')
 
-    const questions = [
+    var questions = [
         {
             question: 'Whos is this Hollywood Actor, made his acting debut in 1990?',
+            image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.XNcJSDv16IdBGeucts-lEQHaE8%26pid%3DApi&f=1',
+            hint: 'He is the first hip-hop star to be nominated for an Oscar for his performances in The Pursuit of Happyness.',
             answers: [
                 {text: 'Will Smith', correct: true},
                 {text: 'Denzel Washington', correct: false},
@@ -18,7 +18,9 @@
            
         },
         {
-            question: 'Which RnB artist left acting to persue career in singing?',
+            question: 'Which RnB artist left acting to pursue a career in singing?',
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl54j9rvyQz48pZet2K-6n7F34ADAcCQjHXg&usqp=CAU',
+            hint: 'Although he is now most famous as a rapper, he was first known for his role on Degrassi: The Next Generation, a popular American TV series.',
             answers: [
                 {text: 'Eminem', correct: false},
                 {text: 'Chris Brown', correct: false},
@@ -27,7 +29,9 @@
             ]
         },
         {
-            question: 'Which Premier League Football Club has won the most Champions League Titles',
+            question: 'Which Premier League Football Club has won the most Champions League Titles?',
+            image: 'https://wordpress-miniclip.s3.amazonaws.com/wp-content/uploads/sites/4/2015/01/Liverpool-FC-2-1024x535.jpg',
+            hint: 'Champions of 2019 champions league, were also crowned for 2019/2020 season of Premier League.',
             answers: [
                 {text: 'Manchester United', correct: false},
                 {text: 'Chelsea', correct: false},
@@ -37,6 +41,8 @@
         },
         {
             question: 'Whos is this talented cricketer, to lead Indian Cricket in all three formats of the game? ',
+            image: 'https://img.etimg.com/thumb/width-1200,height-900,imgsize-184609,resizemode-1,msid-69450227/news/sports/virat-kohli-alone-cant-win-world-cup-sachin-tendulkar.jpg',
+            hint: "In 2020, he is the only player to be named in 'Team of the Decade' in all three formats Cricket.",
             answers: [
                 {text: 'Virat Kohli', correct: true},
                 {text: 'Vikram Solanki', correct: false},
@@ -54,9 +60,10 @@
         setNextQuestion()
         })
 
+        
     $(document).ready(function () {
     $('#show_hint').click(function () {
-        $('#question').toggle();
+        $('#hint').toggle();
         });
     });
     
@@ -69,7 +76,7 @@
         countRightAnswers = 0; 
         setNextQuestion()
         }
-
+ 
     function setNextQuestion() {
         resetState()
         showQuestion(shuffledQuestions[currentQuestion])
@@ -77,6 +84,8 @@
     
     function showQuestion(question) {
         questionAsked.innerText = question.question
+        questionAsked.innerHTML += `<img id="main-image" class="main-image" src="${question.image}" width="500" height="300" alt="image">`
+        hintAsked.innerText = question.hint
         question.answers.forEach(answer => {
             const button = document.createElement('button')
             button.innerText = answer.text
@@ -119,20 +128,23 @@
          document.getElementById('right-answers').innerHTML = (100 * countRightAnswers);
         }
 
-    function setStatusClass(element, correct) {
-        clearStatusClass(element)
+    function setStatusClass(chosen, correct) {
+        clearStatusClass(chosen)
+        var element = document.getElementById("main-image");
+        element.classList.remove("main-image");
         if (correct) {
-            element.classList.add('correct')
+            chosen.classList.add('correct')
             }
             else {
-                element.classList.add('wrong')
+                chosen.classList.add('wrong')
                 }
 
         }
+    
 
-    function clearStatusClass(element) {
-        element.classList.remove('correct')
-        element.classList.remove('wrong')
+    function clearStatusClass(chosen) {
+        chosen.classList.remove('correct')
+        chosen.classList.remove('wrong')
         }
 
      
