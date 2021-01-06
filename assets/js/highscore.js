79c2ccdac40611b1228f8d42f4cf052bfd3495b4
MannyBinning <manib58@live.co.uk>
2021-01-06 21:32:09 +0000

    stopped points from submitting if input is empty.

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,7 +1,7 @@
 const highScoresList = document.querySelector('#scores-list')
 const pointsScored = JSON.parse(localStorage.getItem("pointsScored")) || []
 const savedList = pointsScored.map( function(points){
-    return `<li>${points.name} - ${points.points}</li>`
+    return `<li>${points.name} - ${points.points} - ${points.date}</li>`
 }).join("")
 
 
--- assets/js/points.js
+++ assets/js/points.js
@@ -2,6 +2,7 @@ const endScore = document.querySelector('#points-scored')
 const player = document.querySelector('#player')
 const savePoints = document.querySelector('#savePoints')
 const pointsScored = JSON.parse(localStorage.getItem('pointsScored')) || []
+var date = new Date();
 
 // Final points scored in the quiz to appear on the points page 
 endScore.innerHTML = localStorage.getItem('finalScore');
@@ -11,9 +12,18 @@ endScore.innerHTML = localStorage.getItem('finalScore');
 function savePointsScored(e) {
     e.preventDefault()
 
+//making sure a name is added in input box before
+    if (player.value === "")
+        {
+            alert("You must fill a name.");
+            return false;
+        }
+    window.localStorage.setItem("date", date);
+
     const points = {
         points: localStorage.getItem('finalScore'),
-        name: player.value
+        name: player.value,
+        date: localStorage.getItem('date')
     }
 
     pointsScored.push(points)
