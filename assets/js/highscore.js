bd6fd3be967eac3a559b4ac1369994b922eb7fa7
MannyBinning <manib58@live.co.uk>
2021-01-07 16:28:05 +0000

    organised highscore page List with a grid

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,10 +1,22 @@
-var highScoresList = document.querySelector('#scores-list');
+var highScoresList = document.querySelector('#player-list');
+var pointsList = document.querySelector('#point-list')
+var dateList = document.querySelector('#date-list')
 var pointsScored = JSON.parse(localStorage.getItem("pointsScored")) || [];
 
 // To retreive data and return a list.
 var savedList = pointsScored.map( function(points){
-    return `<li>${points.name} -- ${points.points} -- ${points.date}</li>`;
+    return `<li>${points.name}</li>`;
+}).join("");
+
+var savedPointsList = pointsScored.map( function(points){
+    return `<li>${points.points}</li>`;
+}).join("");
+
+var savedDateList = pointsScored.map( function(points){
+    return `<li>${points.date}</li>`;
 }).join("");
 
 //to display players on the Highscorers List
 highScoresList.innerHTML = savedList;
+pointsList.innerHTML = savedPointsList;
+dateList.innerHTML = savedDateList;
